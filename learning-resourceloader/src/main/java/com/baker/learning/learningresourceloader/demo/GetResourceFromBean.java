09280ea8967bfd94ef6d9b03f2220ab30455e820
Baker-abc <1823023764@qq.com>
2019-12-06 09:13:51 +0800

    spring resourceloader

---
+++ learning-resourceloader/src/main/java/com/baker/learning/learningresourceloader/demo/GetResourceFromBean.java
@@ -0,0 +1,46 @@
+package com.baker.learning.learningresourceloader.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * @description
+ * @date 2019/12/6 8:57
+ */
+@RestController
+public class GetResourceFromBean {
+
+    @Autowired
+    LoaderService loaderService;
+
+    @GetMapping("/resource")
+    public String getResource() {
+//        Class path
+        Resource resourceClassPath =
+                loaderService.getResource("classpath:testing.txt");
+
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            InputStream is = resourceClassPath.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                stringBuffer.append(line).append(";");
+            }
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuffer.toString();
+    }
+}
---
+++ learning-resourceloader/src/test/java/com/baker/learning/learningresourceloader/LearningResourceloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baker.learning.learningresourceloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningResourceloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
