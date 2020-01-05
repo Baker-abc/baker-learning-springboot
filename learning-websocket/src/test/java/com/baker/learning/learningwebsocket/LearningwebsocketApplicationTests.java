05239376ad716589c9218ec1177548e97f468aca
e <18621277021@qq.com>
2020-01-05 12:28:41 +0800

    web-socket

---
+++ learning-websocket/src/test/java/com/baker/learning/learningwebsocket/LearningwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baker.learning.learningwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningwebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
