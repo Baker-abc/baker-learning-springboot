a750dc0048911dff4628ac953f5d6757210b7b1b
Baker-abc <18621277021@qq.com>
2020-01-05 12:28:41 +0800

    web-socket

---
+++ learning-websocket/src/main/java/com/baker/learning/learningwebsocket/LearningwebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.baker.learning.learningwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningwebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningwebsocketApplication.class, args);
+    }
+
+}
